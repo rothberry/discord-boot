8a107632f59cdcc7509fe2c913bd70bbddd2ac19
rothberry <phil.roth077@gmail.com>
2022-06-21 15:23:54 -0700

    deploying

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const fs = require("fs")
 // const path = require("path")
 const { Client, Collection, Intents } = require("discord.js")
-const { token } = require("./config.json")
+// const { token } = require("./config.json")
+require("dotenv").config()
+const { token } = process.env
 
 const client = new Client({
 	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
