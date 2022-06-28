76ac0c77beb2e3aedeb2aea748f5dfafdde463aa
rothberry <phil.roth077@gmail.com>
2022-06-28 14:48:05 -0700

    dynamic yt search

--- commands/play.js
+++ commands/play.js
@@ -1,16 +1,5 @@
-const ytdl = require("ytdl-core")
-const ytSearch = require("yt-search")
-const {
-	joinVoiceChannel,
-	createAudioPlayer,
-	createAudioResource,
-	StreamType,
-} = require("@discordjs/voice")
 const { MessageEmbed } = require("discord.js")
 const { QueryType } = require("discord-player")
-
-// const { OpusEncoder } = require("@discordjs/opus")
-// const encoder = new OpusEncoder(48000, 2)
 
 module.exports = {
 	name: "play",
@@ -24,68 +13,36 @@ module.exports = {
 		if (!queue.connection) await queue.connect(voiceChannel)
 
 		let embed = new MessageEmbed()
-		// const testUrl = "https://www.youtube.com/watch?v=Iczqotmm5sk"
-		if (args[0] === "url") {
-			const result = await client.player.search(args[1], {
-				requestedBy: message.member.user,
-				searchEngine: QueryType.YOUTUBE_VIDEO,
-			})
-			const song = result.tracks[0]
-			console.log({ result, message })
-			await queue.addTrack(song)
+
+		// ! Can handle single song search or url
+		const result = await client.player.search(args.join(" "), {
+			requestedBy: message.member.user,
+			searchEngine: QueryType.AUTO,
+		})
+		if (!!result.playlist) {
+			// if it's Playlist, then add all to queue
+			const {
+				tracks,
+				playlist: { title, url, thumbnail },
+			} = result
+			await queue.addTracks(tracks)
 			embed
-				.setDescription(
-					`**[${song.title}](${song.url})** has been added to the Queue`
-				)
-				.setThumbnail(song.thumbnail)
-				.setFooter({ text: `Duration: ${song.duration}` })
+				.setDescription(`**[${title}](${url})** has been added to the Queue`)
+				.setThumbnail(thumbnail)
+				.setFooter({ text: `Added ${tracks.length}` })
+		} else {
+			const track = result.tracks[0]
+			const { title, url, thumbnail, duration } = track
+			await queue.addTrack(track)
+			embed
+				.setDescription(`**[${title}](${url})** has been added to the Queue`)
+				.setThumbnail(thumbnail)
+				.setFooter({ text: `Duration: ${duration}` })
 		}
+
 		if (!queue.playing) await queue.play()
 		await message.reply({
 			embeds: [embed],
 		})
-	},
-	perform: async (client, message) => {
-		const voiceChannel = message.member.voice.channel
-		if (!voiceChannel) return message.reply("Not in a channel")
-
-		const permissions = voiceChannel.permissionsFor(message.client.user)
-		if (!permissions.has("CONNECT")) return message.reply("Can't connect")
-		if (!permissions.has("SPEAK")) return message.reply("Can't speak..")
-
-		console.log("Summoning into: ", voiceChannel.name)
-		const connection = await joinVoiceChannel({
-			channelId: voiceChannel.id,
-			guildId: message.guild.id,
-			adapterCreator: message.guild.voiceAdapterCreator,
-		})
-		// debugger
-
-		const videoFinder = async (query) => {
-			console.log("finder starting...")
-			const videoResult = await ytSearch(query)
-			console.log({ query, videoResult })
-			return videoResult.videos[0]
-		}
-		const video = await videoFinder("flume say nothing")
-
-		const stream = ytdl(video.url, { filter: "audioonly" })
-		// connection.play(stream, { seek: 0, volume: 1 })
-		const player = createAudioPlayer()
-		// const resource = createAudioResource(video.url)
-		connection.subscribe(player)
-		let resource = createAudioResource(stream, {
-			inputType: StreamType.OggOpus,
-		})
-		resource.encoder = encoder
-
-		console.log("encoder: ", resource.encoder)
-		debugger
-		if (player.checkPlayable()) {
-			player.play(resource)
-			message.reply("Playing: " + "flume say nothing")
-		} else {
-			console.log("Couldn't play...")
-		}
 	},
 }
