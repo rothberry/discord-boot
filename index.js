dabc73a308d44c1ef5881422dc8f6ab0ed5b3368
rothberry <phil.roth077@gmail.com>
2022-10-17 16:47:03 -0700

    fix discordjs versioning crap

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require("fs")
 const {
 	Client,
 	Collection,
-	Intents,
+	GatewayIntentBits,
 	GuildMember,
 	MessageEmbed,
 } = require("discord.js")
@@ -16,9 +16,9 @@ const port = PORT || 8080
 
 const client = new Client({
 	intents: [
-		Intents.FLAGS.GUILDS,
-		Intents.FLAGS.GUILD_MESSAGES,
-		Intents.FLAGS.GUILD_VOICE_STATES,
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.GuildVoiceStates,
 	],
 })
 
